f23b6b69b882f07951fb2ccc7fc635044445f447
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-04-19 23:16:04 +0530

    props

---
+++ text-utils/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+   <>
+ 
+    <Navbar title="logistics" aboutText="Aboutt."/>
+   </>
+  );
+}
+
+export default App;
