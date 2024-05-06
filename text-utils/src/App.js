03dc2065c5563e0b9d2a3e8ce2f3e3018814a5fd
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-05-06 14:39:21 +0530

    adding switch

--- text-utils/src/App.js
+++ text-utils/src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+// import { useState } from 'react';
 function App() {
+  const [mode,setMode]=useState('light'); 
+  const toggleMode=()=>{
+    if(mode=='light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+    }
   return (
+  
    <>
  
-    <Navbar title="logistics" aboutText="Aboutt."/>
+    <Navbar title="logistics" mode={mode} toggleMode={toggleMode}/>
     
-    <div className="container">
-    {/* <TextForm heading="Enter ypur suggestion"/> */}
+    <div className="container my-3">
+     <TextForm heading="Enter the text to analyze below"/>
       <About/>
     </div>
    </>
